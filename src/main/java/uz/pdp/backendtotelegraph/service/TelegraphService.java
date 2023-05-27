2a00bb20ded03c54b2ac787e581aaf0a5e81995e
Bilol665 <bilolsultan665@gmail.com>
2023-05-27 21:57:51 +0500

    It is done

--- src/main/java/uz/pdp/backendtotelegraph/service/TelegraphService.java
+++ src/main/java/uz/pdp/backendtotelegraph/service/TelegraphService.java
@@ -14,9 +14,11 @@ import uz.pdp.backendtotelegraph.repository.TelegraphRepository;
 import uz.pdp.backendtotelegraph.repository.UserRepository;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.function.Supplier;
 
 @Service
 @RequiredArgsConstructor
@@ -29,7 +31,12 @@ public class TelegraphService {
             throw new TelegraphNotCreatedException("Telegraph properties are blank");
         }
         TelegraphEntity map = modelMapper.map(telegraphDto, TelegraphEntity.class);
-        UserEntity userEntity = userRepository.findById(userId).get();
+        UserEntity userEntity;
+        try {
+            userEntity = userRepository.findById(userId).orElseThrow((Supplier<Throwable>) () -> new DataNotException("User not found"));
+        } catch (Throwable e) {
+            throw new DataNotException("User not found!");
+        }
         map.setAuthor(userEntity);
         map.setLink(userEntity.getUsername() + map.getTitle() + LocalDate.now());
         try {
@@ -43,28 +50,43 @@ public class TelegraphService {
             throw new DataNotException("user id not found");
         }
         List<TelegraphEntity> res = new ArrayList<>();
-        List<TelegraphEntity> telegraphEntitiesByAuthor = telegraphRepository.findTelegraphEntitiesByAuthor(userRepository.findById(userId).get());
+        List<TelegraphEntity> telegraphEntitiesByAuthor;
+        try {
+            telegraphEntitiesByAuthor = telegraphRepository.findTelegraphEntitiesByAuthor(userRepository.findById(userId).orElseThrow((Supplier<Throwable>) () -> new DataNotException("User not found")));
+        } catch (Throwable e) {
+            throw new DataNotException("User not found!");
+        }
         int size = telegraphEntitiesByAuthor.size();
         if(size <= pageSize) {
             if(whichPage != 1) {
-                throw new PageNotFoundException("Page not found");
+                throw new PageNotFoundException("Page not found!");
             }
             return telegraphEntitiesByAuthor;
         }
         size/=pageSize;
-        if(size < whichPage) throw new PageNotFoundException("Page not found");
+        if(size < whichPage) throw new PageNotFoundException("Page not found!");
         for (int i = whichPage*pageSize-pageSize; i < whichPage*pageSize; i++) {
             res.add(telegraphEntitiesByAuthor.get(i));
         }
         return res;
     }
 
-    public List<TelegraphEntity> search(String date, String title) {
-        if(date.isBlank()) {
-            return telegraphRepository.findTelegraphEntitiesByTitleOrderByTitleAsc(title);
-        }else if(title.isBlank()) {
-//            return telegraphRepository;
+    public List<TelegraphEntity> search(String date, String title,boolean sortByTitle,boolean sortByDate) {
+        if(date.isBlank() && !title.isBlank()) {
+            if(sortByTitle) {
+                return telegraphRepository.findTelegraphEntitiesByTitleContainsIgnoreCaseOrderByTitleAsc(title);
+            } else {
+                return telegraphRepository.findTelegraphEntitiesByTitleContainsIgnoreCaseOrderByTitleDesc(title);
+            }
+        }else if(title.isBlank() && !date.isBlank()) {
+            LocalDateTime localDateTime = LocalDateTime.parse(date + "T00:00:00");
+            LocalDateTime localDateTime1 = LocalDateTime.of(localDateTime.getYear(),localDateTime.getMonth(),localDateTime.getDayOfMonth()+1,localDateTime.getHour(),localDateTime.getMinute());
+            if(sortByDate) {
+                return telegraphRepository.findTelegraphEntitiesByCreatedDateBetweenOrderByCreatedDateAsc(localDateTime,localDateTime1);
+            } else {
+                return telegraphRepository.findTelegraphEntitiesByCreatedDateBetweenOrderByCreatedDateDesc(localDateTime,localDateTime1);
+            }
         }
-        return null;
+        throw new TelegraphInvalidException("Date is invalid!");
     }
 }
